a720f1fe4ad65ca9d4d8c5a1230b34ab0e9f5ff7
Nathan707264 <nathanpromise999@gmail.com>
2025-07-26 18:53:59 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;HZ8xAYzS#ylB44Kijbn8Hk3gQHfl4RtqKLoC0CId2mmKyrL18X-c", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;GEl0EDRC#YhqjLsAnwvhbAxzXoi5q0Aw_lGDmnAWXzlfH9g3PNcs ", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "2347032190032", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
